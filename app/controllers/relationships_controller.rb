1de9fbccb314aabb40b243dbea9d56f0083f3799
rg27 <gualvezreymar@gmail.com>
2021-06-06 03:17:39 +0800

    Fixed the Follow/Unfollow function of current user

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,11 +10,9 @@ class RelationshipsController < ApplicationController
 		redirect_back(fallback_location: request.referer)
 	end
 
-
 	def destroy
 		Relationship.find_by(follower_id: current_user.id,
 			followed_id: params[:followed_id]).destroy
-			
 		redirect_back(fallback_location: request.referer)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
 	has_secure_password #Equivalent of password and password confirmation
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil:true #It shoud be place after "has_secured_password since it is depending on Bcrypt"
 	has_many :posts
+	has_many :active_relationship, class_name: "Relationship", foreign_key:	"follower_id",  dependent: :destroy
+	has_many :passive_relationship, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followed_users, through: :active_relationships, source: :followed_user
+	has_many :follower_users, through: :passive_relationship, source: :follower_user
+
+	def following?(other_user)
+		Relationship.find_by_followed_id(other_user.id)
+	end
+	
 end
